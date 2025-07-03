eab529f301938701dcdb0e1d9495d567e7433bad
jathorp <jat1963@icloud.com>
2025-07-03 20:18:16 +0100

    Added 03 TF stuff

--- infra/components/02-stateful-resources/outputs.tf
+++ infra/components/02-stateful-resources/outputs.tf
@@ -1,8 +1,11 @@
-# components/02-stateful-resources/outputs.tf
+# components/02-stateful-resources/outputs.tf (CORRECTED)
 
 # S3 Outputs
 output "landing_bucket_id" {
   value = aws_s3_bucket.landing.id
+}
+output "landing_bucket_arn" {
+  value = aws_s3_bucket.landing.arn
 }
 output "archive_bucket_arn" {
   value = aws_s3_bucket.archive.arn
@@ -17,6 +20,12 @@ output "main_queue_arn" {
 output "idempotency_table_name" {
   value = aws_dynamodb_table.idempotency.name
 }
+output "idempotency_table_arn" {
+  value = aws_dynamodb_table.idempotency.arn
+}
 output "circuit_breaker_table_name" {
   value = aws_dynamodb_table.circuit_breaker.name
+}
+output "circuit_breaker_table_arn" {
+  value = aws_dynamodb_table.circuit_breaker.arn
 }

ef917296c531a20cf819c99dc079ea4647080bd2
jathorp <jat1963@icloud.com>
2025-07-03 19:14:12 +0100

    Changed bucket name

--- infra/components/02-stateful-resources/data.tf
+++ infra/components/02-stateful-resources/data.tf
@@ -3,7 +3,7 @@
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket = "data-agregator-tfstate-dev" # This must match the backend config
+    bucket = "data-agregator-tfstate-2-dev" # This must match the backend config
     key    = "dev/components/01-network.tfstate" # The exact key for the network state
     region = "eu-west-2"
   }
--- infra/setup.sh
+++ infra/setup.sh
@@ -1,8 +1,8 @@
 aws s3api create-bucket \
-  --bucket data-agregator-tfstate-dev \
+  --bucket data-agregator-tfstate-2-dev \
   --region eu-west-2 \
   --create-bucket-configuration LocationConstraint=eu-west-2
 
   aws s3api put-public-access-block \
- --bucket data-agregator-tfstate-dev \
+ --bucket data-agregator-tfstate-2-dev \
  --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"

9ca849bd734a563126de4194436dc89cf4f40068
jathorp <jat1963@icloud.com>
2025-07-03 19:34:13 +0100

    Added name to the vpc

--- infra/components/01-network/main.tf
+++ infra/components/01-network/main.tf
@@ -14,7 +14,9 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags                 = local.common_tags
+  tags                 = merge(local.common_tags, {
+    Name = "${var.project_name}-vpc"
+  })
 }
 
 resource "aws_subnet" "private" {

34d215153815b12c6da82d8621cc3d75f38822bd
jathorp <jat1963@icloud.com>
2025-07-03 19:30:11 +0100

    Looking at security for vpc

--- infra/components/01-network/main.tf
+++ infra/components/01-network/main.tf
@@ -22,3 +22,40 @@ resource "aws_subnet" "private" {
   cidr_block = var.subnet_cidr_block
   tags       = merge(local.common_tags, { Name = "${var.project_name}-private-subnet" })
 }
+
+resource "aws_network_acl" "private" {
+  vpc_id = aws_vpc.main.id
+
+  # OUTBOUND RULES: What can leave our subnet?
+  # By default, we will allow our Lambda to initiate traffic to the on-premise network.
+  # This allows outbound traffic on any port to any destination within the private network.
+  # This will be further restricted by the Lambda's Security Group later.
+  egress {
+    protocol   = "-1" # -1 means all protocols
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  # INBOUND RULES: What can enter our subnet?
+  # We only allow "return" traffic from the connections our Lambda made.
+  # This uses ephemeral ports, which is standard for stateful connections.
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  tags = merge(local.common_tags, { Name = "${var.project_name}-private-nacl" })
+}
+
+# Associate our new, secure NACL with our private subnet.
+resource "aws_network_acl_association" "private" {
+  network_acl_id = aws_network_acl.private.id
+  subnet_id      = aws_subnet.private.id
+}

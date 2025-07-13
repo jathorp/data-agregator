92989bddb5969d867e54c30f4a42c7f5ce3fbca1
jathorp <jat1963@icloud.com>
2025-07-13 07:49:52 +0100

    Sm all refactor to lambda - still as simple as possible

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 cd "$(dirname "$0")"
 
 rm -rf build dist
 mkdir -p build dist
 
+echo "🔹 Installing aws-lambda-powertools..."
+uv pip install --upgrade --no-compile --target build aws-lambda-powertools
+
 echo "🔹 Copying package..."
 cp -R src/data_aggregator build/
 
-( cd build && zip -qr ../dist/lambda.zip . )
+(cd build && zip -qr ../dist/lambda.zip .)
 echo "✅ ZIP ready: $(pwd)/dist/lambda.zip"

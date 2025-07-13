6628a58f84347755c5ffbbece918eadf9b3471d6
jathorp <jat1963@icloud.com>
2025-07-13 07:52:18 +0100

    Sm all refactor to lambda - still as simple as possible

--- src/data_aggregator/app.py
+++ src/data_aggregator/app.py
@@ -1,6 +1,10 @@
 # src/app.py          <-- keep it at the top level of the ZIP
 
+from aws_lambda_powertools import Logger
 from data_aggregator.core import health_check
 
+logger = Logger(service="smoke-test")
+
 def handler(event, context):
+    logger.info("Powertools import succeeded!", extra={"event": event})
     return {"core_says": health_check()}
